5c1f7353b34b942c68ff4305df36820618bf8bdf
Kewsito <ksalinas191@gmail.com>
2024-12-23 22:33:33 +0100

    netlify commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,15 @@ import tailwind from '@astrojs/tailwind';
 
 
 
+import netlify from '@astrojs/netlify';
+
+
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://kewsito.github.io',
   base: 'tierra-cuyana',
+  adapter: netlify(),
 });
